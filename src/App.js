1d398198b3dd9317db67daf15dbb17ba6cd85425
Brad Rutkowski <rutkowski.bradley@gmail.com>
2019-04-08 21:56:20 -0400

    Added basic layout and navigation.

--- src/App.js
+++ src/App.js
@@ -1,28 +1,44 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+
+import "bootstrap/dist/css/bootstrap.min.css";
+
+import EventsList from "./components/events-list.component.js";
+import EventDetails from "./components/event-details.component.js";
+import CreateEvent from "./components/create-event.component.js";
+import EditEvent from "./components/edit-event.component.js";
+
 import logo from './logo.svg';
-import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <Router>
+                <div className="container">
+                    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+                        <a class="navbar-brand" href="https://gannon.edu/" target="_blank">
+                            <img src={logo} width="30" height="30" alt="Gannon.edu" />
+                        </a>
+                        <Link to="/" className="navbar-brand">Gannon Events</Link>
+                        <div className="collpase navbar-collapse">
+                            <ul className="navbar-nav mr-auto">
+                                <li className="navbar-item">
+                                    <Link to="/" className="nav-link">Home</Link>
+                                </li>
+                                <li className="navbar-item">
+                                    <Link to="/create" className="nav-link">Create Event</Link>
+                                </li>
+                            </ul>
+                        </div>
+                    </nav>
+                    <br/>
+                    <Route path="/" exact component={EventsList} />
+                    <Route path="/edit/:id" component={EditEvent} />
+                    <Route path="/create" component={CreateEvent} />
+                </div>
+            </Router>
+        );
+    }
 }
 
 export default App;
