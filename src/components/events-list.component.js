43303d021dc10aa06bd8c4acd0201d84d3ef5800
Brad Rutkowski <rutkowski.bradley@gmail.com>
2019-04-26 13:02:26 -0400

    Changed events-list to display events in a card layout instead of a table layout. Added MaterialUI as a dependency to create the card layout.

--- src/components/events-list.component.js
+++ src/components/events-list.component.js
@@ -2,7 +2,15 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import axios from 'axios';
 
-const Event = props => (
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+
+/*const Event = props => (
     <tr>
         <td>{props.event.event_name}</td>
         <td>{props.event.event_desc}</td>
@@ -13,6 +21,49 @@ const Event = props => (
             <Link to={"/edit/"+props.event._id}>Edit</Link>
         </td>
     </tr>
+)*/
+
+const styles = {
+    cards: {
+        marginBottom: '20px',
+        width: '350px',
+    },
+    media: {
+        display: 'block',
+        maxWidth: '350px',
+        width: 'auto',
+        height: 'auto',
+    }
+}
+
+const Event = props => (
+    <Card style={styles.cards}>
+        <Link to={"/edit/"+props.event._id}>
+            <CardActionArea>
+                <CardMedia
+                    component="img"
+                    alt="Contemplative Reptile"
+                    height="140"
+                    image={require("../resources/ErieTower.jpg")}
+                    title="Contemplative Reptile"
+                    style={styles.media}
+                />
+                <CardContent>
+                    <Typography gutterBottom variant="h5" component="h2">
+                        {props.event.event_name}
+                    </Typography>
+                    <Typography component="p">
+                        {props.event.event_desc}
+                    </Typography>
+                </CardContent>
+            </CardActionArea>
+        </Link>
+        <CardActions>
+            <Button size="small" color="primary">
+                Learn More
+            </Button>
+        </CardActions>
+    </Card>
 )
 
 export default class EventsList extends Component {
@@ -42,20 +93,9 @@ export default class EventsList extends Component {
         return (
             <div>
                 <h3>Events List</h3>
-                <table className="table table-striped" style={{ marginTop: 20 }} >
-                    <thead>
-                        <tr>
-                            <th>Name</th>
-                            <th>Description</th>
-                            <th>Date</th>
-                            <th>Start Time</th>
-                            <th>End Time</th>
-                        </tr>
-                    </thead>
-                    <tbody>
+                    <div>
                         { this.eventList() }
-                    </tbody>
-                </table>
+                    </div>
             </div>
         )
     }
